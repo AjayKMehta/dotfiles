0a7bc237112a101c636ec3c8eed26f18c210a9ff
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2023-03-19 17:15:39 -0700

    More cleanup

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,19 +1,37 @@
 local launch_menu = {}
 local wezterm = require 'wezterm';
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  table.insert(launch_menu, {
-    label = "PowerShell",
-    args = { "powershell.exe", "-NoLogo" }
-  }
+  table.insert(
+    launch_menu,
+    {
+      label = wezterm.format {
+        { Attribute = { Intensity = "Bold" } },
+        { Text = "PowerShell Core" }
+      },
+      args = { "pwsh.exe", "-NoLogo", "-NoExit" }
+    }
   )
 
-  table.insert(launch_menu, {
-    label = wezterm.format {
-      { Attribute = { Intensity = "Bold" } },
-      { Text = "pwsh" }
-    },
-    args = { "pwsh.exe", "-NoLogo" }
-  }
+  local set_environment_variables = {}
+  set_environment_variables['prompt'] =
+  '$E]7;file://localhost/$P$E\\$E[32m$T$E[0m $E[35m$P$E[36m$_$G$E[0m '
+  -- use a more ls-like output format for dir
+  set_environment_variables['DIRCMD'] = '/d'
+
+  table.insert(
+    launch_menu,
+    {
+      label = 'cmd',
+      set_environment_variables = set_environment_variables,
+      args = { 'cmd.exe' }
+    }
+  )
+  table.insert(
+    launch_menu,
+    {
+      label = "Windows PowerShell",
+      args = { "powershell.exe", "-NoLogo" }
+    }
   )
 end
 
@@ -28,7 +46,7 @@ wezterm.on('update-status', function(window, pane)
 end)
 
 return {
-  color_scheme = "Aurora",
+  color_scheme = "Dracula+",
   default_prog = { "pwsh" },
   font = wezterm.font {
     family = 'JetBrains Mono',
@@ -45,10 +63,6 @@ return {
     border_left_width = '0.5cell',
     border_right_width = '0.5cell',
     border_bottom_height = '0.25cell',
-    border_top_height = '0.25cell',
-    border_left_color = '#FAEE00',
-    border_right_color = '#FAEE00',
-    border_bottom_color = '#FAEE00',
-    border_top_color = '#FAEE00',
+    border_top_height = '0.25cell'
   }
 }
