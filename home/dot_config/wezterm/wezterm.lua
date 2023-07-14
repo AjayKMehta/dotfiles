bba7d8394f881f59b61e8bbe5d614df054479324
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2023-07-13 20:26:48 -0700

    Updat wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,5 +1,43 @@
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = "Dracula+"
+config.enable_scroll_bar = true
+config.default_prog = { "pwsh" }
+config.font = wezterm.font_with_fallback({
+  'DejaVuSansM Nerd Font Mono',
+  -- <built-in>, BuiltIn
+  "JetBrains Mono",
+
+  -- <built-in>, BuiltIn
+  -- Assumed to have Emoji Presentation
+  -- Pixel sizes: [128]
+  "Noto Color Emoji",
+
+  -- <built-in>, BuiltIn
+  "Symbols Nerd Font Mono",
+})
+-- By default, SHIFT-PageUp and SHIFT-PageDown will adjust the viewport
+-- scrollback position by one full screen for each press.
+config.enable_scroll_bar = true
+config.scrollback_lines = 3500
+config.window_frame = {
+  border_left_width = '0.5cell',
+  border_right_width = '0.5cell',
+  border_bottom_height = '0.25cell',
+  border_top_height = '0.25cell'
+}
+
+
 local launch_menu = {}
-local wezterm = require 'wezterm';
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   table.insert(
     launch_menu,
@@ -31,6 +69,7 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   )
 end
 
+config.launch_menu = launch_menu
 wezterm.on('update-status', function(window, pane)
   local overrides = window:get_config_overrides() or {}
   if window:is_focused() then
@@ -41,24 +80,4 @@ wezterm.on('update-status', function(window, pane)
   window:set_config_overrides(overrides)
 end)
 
-return {
-  color_scheme = "Dracula+",
-  default_prog = { "pwsh" },
-  font = wezterm.font {
-    family = 'JetBrains Mono',
-    stretch = 'Expanded',
-    weight = 'Regular',
-    assume_emoji_presentation = true
-  },
-  launch_menu = launch_menu,
-  -- By default, SHIFT-PageUp and SHIFT-PageDown will adjust the viewport
-  -- scrollback position by one full screen for each press.
-  enable_scroll_bar = true,
-  scrollback_lines = 3500,
-  window_frame = {
-    border_left_width = '0.5cell',
-    border_right_width = '0.5cell',
-    border_bottom_height = '0.25cell',
-    border_top_height = '0.25cell'
-  }
-}
+return config
