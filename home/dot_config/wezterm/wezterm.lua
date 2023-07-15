defc5438be7d4d9771ab9303d569a5cdb6144e05
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2023-07-14 21:09:06 -0700

    Update window_frame config.

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -35,9 +35,27 @@ config.window_frame = {
   border_left_width = '0.5cell',
   border_right_width = '0.5cell',
   border_bottom_height = '0.25cell',
-  border_top_height = '0.25cell'
-}
+  border_top_height = '0.25cell',
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font { family = 'Roboto', weight = 'Bold' },
 
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 12.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#00ddff',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#333333',
+}
 
 local launch_menu = {}
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
@@ -102,4 +120,5 @@ wezterm.on('augment-command-palette', function(window, pane)
     },
   }
 end)
+
 return config
