2c841dc8344ab313c11717b11d323fb8b0708e6e
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-06-22 15:59:56 -0700

    Modify OMP setup for cmd to set theme based on $CONDA_PREFIX being set or not.

--- home/AppData/Local/clink/oh-my-posh.lua
+++ home/AppData/Local/clink/oh-my-posh.lua
@@ -1,2 +1,9 @@
 -- https://ohmyposh.dev/docs/installation/prompt
-load(io.popen('oh-my-posh init cmd'):read("*a"))()
+
+local conda_theme = "%LocalAppData%/Programs/oh-my-posh/themes/tonybaloney.omp.json"
+local regular_theme = "%LocalAppData%/Programs/oh-my-posh/themes/negligible.omp.json"
+
+-- Choose theme based on whether CONDA_PREFIX is set
+local theme = os.getenv("CONDA_PREFIX") == nil and regular_theme or conda_theme
+
+load(io.popen('oh-my-posh init cmd -c=' .. theme):read("*a"))()
