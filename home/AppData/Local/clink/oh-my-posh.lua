95c5cce59fd3d226aa2571c661f2ede021a3cd9a
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2025-03-06 16:40:38 -0800

    OMP fixes

--- home/AppData/Local/clink/oh-my-posh.lua
+++ home/AppData/Local/clink/oh-my-posh.lua
@@ -1,9 +1,9 @@
 -- https://ohmyposh.dev/docs/installation/prompt
 
-local conda_theme = "%LocalAppData%/Programs/oh-my-posh/themes/tonybaloney.omp.json"
-local regular_theme = "%LocalAppData%/Programs/oh-my-posh/themes/negligible.omp.json"
+local conda_theme = os.getenv("POSH_THEMES_PATH") .. "tonybaloney.omp.json"
+local regular_theme = os.getenv("POSH_THEMES_PATH") .. "negligible.omp.json"
 
 -- Choose theme based on whether CONDA_PREFIX is set
 local theme = os.getenv("CONDA_PREFIX") == nil and regular_theme or conda_theme
-
-load(io.popen('oh-my-posh init cmd -c=' .. theme):read("*a"))()
+print(theme)
+load(io.popen('oh-my-posh init cmd -c="' .. theme ..'"'):read("*a"))()
