53da282117f04f5ec4660c16c37979da4a06a4e3
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2026-05-23 09:11:36 -0700

    Remove aichat.

--- home/Documents/PowerShell/Scripts/PSReadLine/PSReadLineProfile.ps1
+++ home/Documents/PowerShell/Scripts/PSReadLine/PSReadLineProfile.ps1
@@ -599,15 +599,3 @@ if (!$isVsCode) {
 }
 
 #endregion
-
-# https://github.com/sigoden/aichat/blob/main/scripts/shell-integration/integration.ps1
-Set-PSReadLineKeyHandler -Chord "alt+e" -ScriptBlock {
-    $_old = $null
-    [Microsoft.PowerShell.PSConsoleReadline]::GetBufferState([ref]$_old, [ref]$null)
-    if ($_old) {
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert('⌛')
-        $_new = (aichat -e $_old)
-        [Microsoft.PowerShell.PSConsoleReadLine]::DeleteLine()
-        [Microsoft.PowerShell.PSConsoleReadline]::Insert($_new)
-    }
-}
