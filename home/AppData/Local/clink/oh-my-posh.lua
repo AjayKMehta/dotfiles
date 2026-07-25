48a41c77181204d9313443afdf2776ee8a557d76
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2026-07-25 10:17:34 -0700

    Update clink settings.

--- home/AppData/Local/clink/oh-my-posh.lua
+++
@@ -1,9 +0,0 @@
--- https://ohmyposh.dev/docs/installation/prompt
-
-local conda_theme = os.getenv("POSH_THEMES_PATH") .. "tonybaloney.omp.json"
-local regular_theme = os.getenv("POSH_THEMES_PATH") .. "negligible.omp.json"
-
--- Choose theme based on whether CONDA_PREFIX is set
-local theme = os.getenv("CONDA_PREFIX") == nil and regular_theme or conda_theme
-print(theme)
-load(io.popen('oh-my-posh init cmd -c="' .. theme ..'"'):read("*a"))()
