056ecb065ecdf69510114e4f6f703e47246b79a3
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2026-08-09 11:18:33 -0700

    Update VS Code PowerShell profile.

--- home/Documents/PowerShell/Microsoft.VSCode_profile.ps1
+++ home/Documents/PowerShell/Microsoft.VSCode_profile.ps1
@@ -1,4 +1,4 @@
-Set-PoshPrompt -Theme "$env:LOCALAPPDATA\Programs\oh-my-posh\themes\bubblesextra.omp.json"
+(oh-my-posh init pwsh --config bubblesextra) | Invoke-Expression
 
 # https://github.com/SeeminglyScience/EditorServicesCommandSuite/issues/45#issuecomment-596243308
 Import-Module EditorServicesCommandSuite
